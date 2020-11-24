026517c3a0f6bf6f5d672f60a01fa5ac49e3da25
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-24 17:21:26 +0000

    updated README

--- environment/provision.sh
+++ environment/provision.sh
@@ -2,3 +2,13 @@
 
 sudo apt-get update
 sudo apt-get install nginx -y
+sudo apt install mongodb -y
+sudo systemctl start mongodb
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo npm install pm2
+sudo npm install mongoose
+sudo npm install faker
+cd /app
+npm install
+npm start &
